1d868e21a3fe9da576fa4f2befc38f04df66d3ac
LAHAXE Arnaud <lahaxe.arnaud@gmail.com>
2021-12-31 13:54:59 +0100

    Phpstan

--- src/ContextProviderInterface.php
+++ src/ContextProviderInterface.php
@@ -4,6 +4,9 @@ namespace Alahaxe\HealthCheckBundle\Contract;
 
 interface ContextProviderInterface
 {
+    /**
+     * @return string|array<mixed>|int|float|\JsonSerializable
+     */
     public function getValue():string|array|int|float|\JsonSerializable;
     public function getName():string;
 }
